8f490df00adc29d6c68d3364a549d9e922b2368d
PavelSapegin <pawsapegin@yandex.ru>
2026-08-04 22:19:44 +0300

    Add script to download dataset

---
+++ src/download.py
@@ -0,0 +1,19 @@
+import os
+import shutil
+
+import kagglehub
+
+# Download latest version
+path = kagglehub.dataset_download("mlg-ulb/creditcardfraud")
+
+target_dir = "./data"
+os.makedirs(target_dir, exist_ok=True)
+
+for file_name in os.listdir(path):
+    full_file_name = os.path.join(path, file_name)
+
+    if os.path.isfile(full_file_name):
+        shutil.copy(full_file_name, target_dir)
+
+
+print(f"Files copied to {os.path.abspath(target_dir)}")
