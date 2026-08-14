463221acb79bf4dde21d577b2caf3b5b6b413e86
PavelSapegin <pawsapegin@yandex.ru>
2026-08-14 12:28:34 +0300

    Add evaluation for autoencoder

--- src/evaluation.py
+++ src/evaluation.py
@@ -1,4 +1,7 @@
+import numpy as np
 import pandas as pd
+import torch
+import torch.nn as nn
 from sklearn.base import BaseEstimator
 from sklearn.metrics import average_precision_score
 
@@ -10,3 +13,17 @@ def evaluate(model: BaseEstimator, x_val: pd.DataFrame, y_val: pd.Series):
     pr_auc = average_precision_score(y_val, anomaly_score)
 
     return pr_auc
+
+
+def evaluate_autoencoder(model: nn.Module,
+                        X_val: torch.Tensor,
+                        y_val: pd.Series,
+                        device: torch.device | str = "cpu") -> tuple[float, np.ndarray]:
+    model.eval()
+    with torch.no_grad():
+        X_val_tensor = X_val.to(device)
+        decoded = model(X_val_tensor)
+        reconstruction_error = torch.mean((X_val_tensor - decoded)**2,1).cpu().numpy()
+        pr_auc = average_precision_score(y_val, reconstruction_error)
+
+    return pr_auc, reconstruction_error
