323cbc51c39e47a465fb8aa5b7b0c82363b6638a
PavelSapegin <pawsapegin@yandex.ru>
2026-08-17 20:36:17 +0300

    Formatting code

--- app/main.py
+++ app/main.py
@@ -13,17 +13,17 @@ from .schemas import BatchRequest, BatchResponse
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     app.state.preprocessing_pipeline = joblib.load(config.COLUMN_TRANSFORMER_PATH)
-    app.state.model = load_model(config.MODEL_WEIGHTS_PATH,
-                                 config.INPUT_DIM,
-                                 config.LATENT_DIM)
+    app.state.model = load_model(
+        config.MODEL_WEIGHTS_PATH, config.INPUT_DIM, config.LATENT_DIM
+    )
 
     yield
 
     app.state.preprocessing_pipeline = None
-    app.state.model =None
+    app.state.model = None
+
 
 app = FastAPI(lifespan=lifespan)
-
 
 
 @app.post("/predict", response_model=BatchResponse)
@@ -32,16 +32,13 @@ def predict(payload: BatchRequest, request: Request):
         responses = predict_batch(
             payload.transactions,
             request.app.state.model,
-            request.app.state.
-            preprocessing_pipeline,
-            config.THRESHOLD
+            request.app.state.preprocessing_pipeline,
+            config.THRESHOLD,
         )
     except (KeyError, ValueError) as e:
         raise HTTPException(
             status_code=400,
-            detail=
-            "Preprocessing/inference error: "
-            f"{e}",
+            detail=f"Preprocessing/inference error: {e}",
         ) from e
 
     return BatchResponse(responses=responses)
